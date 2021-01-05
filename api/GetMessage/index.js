7aa6cd815986e2713c7da78c40965fafa57a1af2
Misha Shaposhnikov <mishapos@microsoft.com>
2021-01-05 15:37:45 -0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: { text: "Hello from the API 1.1" },
+    body: { text: "Hello from the ReactSite 3 API 1.1" },
   };
 };
