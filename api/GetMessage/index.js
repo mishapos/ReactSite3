af62d59e773fda431d2613d288a994b93aa746da
Michail Shaposhnikov <mishapos@microsoft.com>
2021-04-07 11:10:53 -0700

    Removed API

--- api/GetMessage/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function (context, req) {
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: { text: "Hello from the ReactSite 3 API 1.1" + (new Date()) },
-  };
-};
